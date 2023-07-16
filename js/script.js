f74231e94a007ce712c4395ef46b938ec7692917
kennjidr <kennji.dr315@gmail.com>
2023-07-17 06:04:49 +0900

    修正1回目

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  
   var topBtn = $('.pagetop');
   topBtn.hide();
 
@@ -22,7 +23,28 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }, 150, 'swing');
     return false;
   });
+    // フッター手前でストップ
+    $(".pagetop").hide();
+    $(window).on("scroll", function () {
+      scrollHeight = $(document).height();
+      scrollPosition = $(window).height() + $(window).scrollTop();
+      footHeight = $(".footer").innerHeight();
+      if (scrollHeight - scrollPosition <= footHeight) {
+   　　　// ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
+        $(".pagetop").css({
+          position: "absolute",
+          bottom: footHeight,
+        });
+      } else {
+        $(".pagetop").css({
+          position: "fixed",
+          bottom: "0",
+        });
+      }
+    });
 
+
+  
   //ドロワーメニュー
   $("#MenuButton").click(function () {
     // $(".l-drawer-menu").toggleClass("is-show");
@@ -39,10 +61,12 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       ($('.js-drawer-menu').removeClass('is-open'))//js-hamburgerからis-openを持っていた場合
       $(this).removeClass('is-open');//クラスjs-hamburgerからis-openを削除する。
       $('.js-header').removeClass('is-open');//クラスjs-headerからis-openを削除する。
+      $('body').removeClass('is-open');//クラスjs-headerからis-openを削除する。
     } else {
       ($('.js-drawer-menu').addClass('is-open'))//js-hamburgerがクラスis-openを持っていた場合
       $(this).addClass('is-open');//クラスis-openを付与する。
       $('.js-header').addClass('is-open');//クラスis-openを付与する。
+      $('body').addClass('is-open');//クラスis-openを付与する。
     }
   });
 
@@ -58,37 +82,34 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     return false;
   });
 
-  //inview 
-  //要素の取得とスピードの設定
-  var box = $('information-colorobox'),
+  //inview
+  //要素の取得とアニメーションスピードの設定
+  var box = $('.colorbox'),
     speed = 700;
 
-  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  //.colorboxクラスに下記処理を行う
   box.each(function () {
-    $(this).append('<div class="color"></div>')
-    var color = $(this).find($('.color')),
-      image = $(this).find('img');
-    var counter = 0;
+    $(this).append('<div class="colorbox-image"></div>')//.colorboxに<div class="colorbox-image"></div>を付与
+    var color = $(this).find($('.colorbox-image')),
+      image = $(this).find('img');//追加した<div class="colorbox-image"></div>と<img>要素を取得します
+    var counter = 0;//counterがゼロのときに動き出す。ここで稼働回数を変更できる。
 
-    image.css('opacity', '0');
-    color.css('width', '0%');
+    image.css('opacity', '0');//画層を透明にする。
+    color.css('width', '0%');//color要素の幅を0%に設定します。
     //inviewを使って背景色が画面に現れたら処理をする
     color.on('inview', function () {
       if (counter == 0) {
-        $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
-          image.css('opacity', '1');
-          $(this).css({ 'left': '0', 'right': 'auto' });
-          $(this).animate({ 'width': '0%' }, speed);
+        $(this).delay(200).animate({ 'width': '100%' }, speed, function () {//color要素の幅がアニメーションとして100%に拡大
+          image.css('opacity', '1');//画像が出てくる
+          $(this).css({ 'left': '0', 'right': 'auto' });//color要素の位置が左端に移動します
+          $(this).animate({ 'width': '0%' }, speed);///color要素の幅が0になる
         })
-        counter = 1;
+        counter = 1;//counterが1に更新される、これでアニメーションは終わる
       }
     });
   });
-
-
-
-
 });
+
 
 var mySwiper = new Swiper('.swiper-first-view', {
 
@@ -96,9 +117,9 @@ var mySwiper = new Swiper('.swiper-first-view', {
   loopedSliders: 3,//スライド枚数
   speed: 1500,//切り替わるスピード
   effect: 'fade',//フェイドエフェクトの追加
-  fadeEffect: {         //エフェクトの挙動を追加
-    crossFade: true     //追加 (スライドの重なりを解消)
-  },                    //
+  fadeEffect: {       //エフェクトの挙動を追加
+  crossFade: true     //追加 (スライドの重なりを解消)
+  },                  //
   autoplay: {
     delay: 2000,//自動切り替わり速度
   },
@@ -109,23 +130,19 @@ var mySwiper = new Swiper('.campaign__swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  pagination: {
+  
+  　pagination: {
     clickable: true,
   },
   loop: true,
   width: 280,
   spaceBetween: 20,
-  loopedSliders: 3,
+  loopedSliders: 8,
 
   breakpoints: {
     768: {
       width: 333,
       spaceBetween: 40,
-
     }
   },
 });
-
-
-
-
